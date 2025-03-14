bc04e966fd486f5db2a8f6e9eb39d78a90ba1dd6
lazydeveloperprabir <lazydeveloperprabir@gmail.com>
2025-03-14 23:39:48 +0530

    commit -2

--- Bit-Manipulation/AddBinary.java
+++ Bit-Manipulation/AddBinary.java
@@ -31,10 +31,16 @@ Each string does not contain leading zeros except for the zero itself.
 
         while(i >=0 || j >= 0||carry >0){
             int bitA =  i>=0 ? a.charAt(i) -'0' :0;
-            int bitB
+            int bitB = j>=0 ?b.charAt(j) -'0' : 0;
+            int sum = bitA+bitB+carry;
+
+            sb.append(sum % 2);
+            carry = sum/2;
+            i--;
+            j--;
+
         }
+
+        return sb.reverse().toString();
     }
 }
-
-  git config --global user.email "lazydeveloperprabir@gmail.com"
-  git config --global user.name "lazydeveloperprabir"
